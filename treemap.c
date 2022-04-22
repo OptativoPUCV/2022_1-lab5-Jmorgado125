911abe04f46ad8f933abec32ad8c81f45c7d3a83
JMorgado125 <101432493+Jmorgado125@users.noreply.github.com>
2022-04-22 15:39:59 +0000

    Funcion crear mapa corregir

--- treemap.c
+++ treemap.c
@@ -37,9 +37,21 @@ TreeNode * createTreeNode(void* key, void * value) {
 }
 
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
-
+  TreeMap * aux=(TreeMap *)malloc(sizeof(TreeMap));
+  TreeNode * aux->root= createTreeNode();
+  TreeNode * aux->current=createTreeNode();
+  int valor=aux->lower_than(key1,key2);
+  if(valor==1){
+    aux->root=key2;
+    aux->root->left=key1;
+  }
+  else
+  {
+    aux->root=key1;
+    aux->root->left=key2;
+  }
     //new->lower_than = lower_than;
-    return NULL;
+    return aux;
 }
 
 
