8d81fc4b9eea0d9642c2b5cc824aa9d8537de9c7
Your Name <you@example.com>
2022-04-24 23:33:01 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -59,8 +59,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
   while(tree->current != NULL){
     if(tree->lower_than(key,tree->current->pair->key)==1){
       tree->current=tree->current->left;
-      if(tree->current->left == NULL){
-          tree->current->left=nodo;
+      if(tree->current== NULL){
           tree->current=nodo;
           break;
        }
@@ -68,8 +67,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value) {
 
       else if(tree->lower_than(key,tree->current->pair->key)==0){
         tree->current=tree->current->right;
-        if(tree->current->right == NULL){
-          tree->current->right=nodo;
+        if(tree->current== NULL){
           tree->current=nodo;
           break;
         }
